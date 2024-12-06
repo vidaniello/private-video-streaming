4dbc2b49ce52bc0a3dfc56e46bf21f25c08462ba
Vincenzo D'Aniello <vidaniello@gmail.com>
2024-12-06 17:28:09 +0100

    progress develop

--- src/http.ts
+++ src/http.ts
@@ -1,5 +1,21 @@
 import express, {Express, NextFunction, Request, Response} from 'express';
 import {StatusCodes} from 'http-status-codes';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import {HomePage, LoginPage} from './statics';
+
+
+const httpListenPort:Number = 8080; 
+
+class User {
+    name: string = '';
+}
+
+declare module 'express-session' {
+    interface SessionData {
+        user : User;
+    }
+}
 
 export class Http{
 
@@ -9,19 +25,32 @@ export class Http{
 
         Http.app = express();
 
-        Http.app.use(express.json());
+        //Http.app.use(express.json());
+        Http.app.use(cookieParser());
+
+        Http.app.use(session(
+            {
+                secret: '33jj3hj4jk238',
+                resave: false,
+                saveUninitialized: true
+            }
+        ));
 
         Http.app.set('etag', false);
 
         /**Initial interceptor */
         Http.app.use((req, res, next) => {
-            res.set('Cache-Control', 'no-cache, no-store, max-age=0');
-            res.set('Content-Type', 'application/json; charset=utf-8');
+            let accept:String = req.headers.accept;
+            if(accept!==undefined) if(accept!==null)
+                if(accept.includes('application/json')){
+                    res.set('Cache-Control', 'no-cache, no-store, max-age=0');
+                    res.set('Content-Type', 'application/json; charset=utf-8');
+                }
             next()
         });
         
-        Http.app.listen(8080, ()=>{
-            console.log("Http-server listen on port "+8080);
+        Http.app.listen(httpListenPort, ()=>{
+            console.log("Http-server listen on port "+httpListenPort);
         });
 
         Http.app.get('/', Http.onHomePage);
@@ -35,12 +64,22 @@ export class Http{
     }
 
     static onHomePage(req: Request, resp: Response){
-        resp.send({appStatus: "Ok! "+new Date(), environment: 'DEVELOP'});
+        let usr = req.session.user;
+        if(usr==undefined){
+            usr = new User();
+            usr.name = 'nome';
+            req.session.user = usr;
+        }
+        resp.send(HomePage.body());
     }
+
 
     
     static onException(exception: Error | any, resp: Response){
         resp.status(StatusCodes.BAD_REQUEST)
             .send({exceptionType: exception.name, exceptionCode: exception.code, exceptionMessage: exception.message/*, stack: JSON.stringify(error.stack)*/});
     }
+
+
+ 
 }
---
+++ src/statics.ts
@@ -0,0 +1,54 @@
+
+
+export class HomePage {
+    public static body() {
+        return `
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Private video streaming platform</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    
+</head>
+<body>
+    <h1>Private video streaming platform</h1>
+    <video id="videoPlayer" controls style="height: 500px;">
+        <source src="http://localhost:8000/video" type="video/mp4">
+    </video>
+</body>
+</html>
+`
+    }
+}
+
+export class LoginPage {
+    public static body() {
+        return `
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Private video streaming platform</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    
+</head>
+<body>
+    <h1>Private video streaming platform</h1>
+    <div style="width: 260px;">
+        <p>Login</p>
+        <form method="POST" action="/login">
+            <div style="margin: 5px;">username: <input type="text" name="username"></div>
+            <div style="margin: 5px;">password: <input type="password" name="username"></div>
+            <div style="width: 100%; text-align: center;">
+                <input style="width: 50%;" type="submit" value="login">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
+`
+    }
+}
