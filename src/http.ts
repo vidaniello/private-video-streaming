75f6a9a36eac8ab3df2396cffb9c2af01f2493d0
vidaniello <vidaniello@gmail.com>
2024-12-08 00:11:06 +0100

    develop progress

--- src/http.ts
+++ src/http.ts
@@ -2,18 +2,16 @@ import express, {Express, NextFunction, Request, Response} from 'express';
 import {StatusCodes} from 'http-status-codes';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import dotenv from 'dotenv';
+import { LoginStatus } from './common';
 import {HomePage, LoginPage} from './statics';
 
 
 const httpListenPort:Number = 8080; 
 
-class User {
-    name: string = '';
-}
-
 declare module 'express-session' {
     interface SessionData {
-        user : User;
+        loginStatus : LoginStatus;
     }
 }
 
@@ -23,14 +21,19 @@ export class Http{
 
     public static async init(){
 
+        dotenv.config();
+
         Http.app = express();
 
+        Http.app.use(express.urlencoded({ extended: true }));
         //Http.app.use(express.json());
         Http.app.use(cookieParser());
 
+        let ff = process.env.expressSessionKey;
+
         Http.app.use(session(
             {
-                secret: '33jj3hj4jk238',
+                secret: process.env.expressSessionKey,
                 resave: false,
                 saveUninitialized: true
             }
@@ -55,6 +58,9 @@ export class Http{
 
         Http.app.get('/', Http.onHomePage);
 
+        Http.app.post('/login', Http.onLoginPage);
+
+
         /* final interceptor
         Http.app.use((req, res, next) => {
             let i = 0;
@@ -64,16 +70,40 @@ export class Http{
     }
 
     static onHomePage(req: Request, resp: Response){
-        let usr = req.session.user;
-        if(usr==undefined){
-            usr = new User();
-            usr.name = 'nome';
-            req.session.user = usr;
+        let logStat:LoginStatus = Http.getLoginStatus(req);
+        let message:string = "";
+        if(logStat.logged) {
+            message = "Hi "+logStat.username+"!";
+            resp.send(HomePage.body(message));
+        } else {
+            message = "Not Logged in";
+            resp.send(LoginPage.body(message));
         }
-        resp.send(HomePage.body());
     }
 
+    static onLoginPage(req: Request, resp: Response){
+        let logStat:LoginStatus = Http.getLoginStatus(req);
+        let username = req.body.username;
+        let pswd = req.body.password;
+        if(username!==undefined && pswd!==undefined){
+            if(username!=process.env.loginusername && pswd!=process.env.loginpassword)
+                resp.send(LoginPage.body("<span style=\"color: red;\">Wrong username or password!</span>"));
+            else {
+                logStat.logged = true;
+                logStat.username = username;
+                resp.redirect("/");
+            }
+        } else 
+            //Http.onHomePage(req, resp);
+            resp.redirect("/");
+    }
 
+    static getLoginStatus(req: Request) :LoginStatus{
+        let logStatus:LoginStatus = req.session.loginStatus;
+        if(logStatus==undefined || logStatus==null)
+            req.session.loginStatus = new LoginStatus();
+        return req.session.loginStatus;
+    }
     
     static onException(exception: Error | any, resp: Response){
         resp.status(StatusCodes.BAD_REQUEST)
--- src/statics.ts
+++ src/statics.ts
@@ -1,7 +1,7 @@
 
 
 export class HomePage {
-    public static body() {
+    public static body(logged:string) {
         return `
 <!DOCTYPE html>
 <html>
@@ -10,10 +10,10 @@ export class HomePage {
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Private video streaming platform</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    
 </head>
 <body>
     <h1>Private video streaming platform</h1>
+    <div>${logged}</div>
     <video id="videoPlayer" controls style="height: 500px;">
         <source src="http://localhost:8000/video" type="video/mp4">
     </video>
@@ -24,7 +24,7 @@ export class HomePage {
 }
 
 export class LoginPage {
-    public static body() {
+    public static body(message:string) {
         return `
 <!DOCTYPE html>
 <html>
@@ -37,11 +37,16 @@ export class LoginPage {
 </head>
 <body>
     <h1>Private video streaming platform</h1>
+        <div>${message}</div>
     <div style="width: 260px;">
         <p>Login</p>
         <form method="POST" action="/login">
-            <div style="margin: 5px;">username: <input type="text" name="username"></div>
-            <div style="margin: 5px;">password: <input type="password" name="username"></div>
+            <div style="margin: 5px;">
+                username: <input type="text" name="username">
+            </div>
+            <div style="margin: 5px;">
+                password: <input type="password" name="password">
+            </div>
             <div style="width: 100%; text-align: center;">
                 <input style="width: 50%;" type="submit" value="login">
             </div>
